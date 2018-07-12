016a587208ac33de67757e5d2a7b428978f43fcd
moahmmednms <41140381+moahmmednms@users.noreply.github.com>
2018-07-12 14:13:08 +0530

    Create leap_or_not.py

---
+++ leap_or_not.py
@@ -0,0 +1,12 @@
+leap=int(input())
+if(leap%4==0):
+    if(leap%100==0):
+        if(leap%400!=0):
+            print("yes")
+        else:
+            print("No")
+    else:
+        print("yes")
+else:
+    print("No")
+    
