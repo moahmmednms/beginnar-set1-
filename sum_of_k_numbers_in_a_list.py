f9d886bd0c4d85a3d85c84de0c23a090ac01977e
moahmmednms <41140381+moahmmednms@users.noreply.github.com>
2018-07-12 15:10:19 +0530

    Update sum_of_k_numbers_in_a_list.py

--- sum_of_k_numbers_in_a_list.py
+++ sum_of_k_numbers_in_a_list.py
@@ -1,12 +1,8 @@
-n=input()
-b=n.split()
+a=input()
+b=a.split()
+c=input()
+d=c.split()
 sum=0
-list=[]
-for i in range(int(b[0])):
-    a=int(input())
-    list.append(a)
 for i in range(int(b[1])):
-    sum=sum+list[i]
+    sum=sum+int(d[i])
 print(sum)
-
-
