cf4c74ecf37a7910efdb9d3e22681cb3dc4776c0
moahmmednms <41140381+moahmmednms@users.noreply.github.com>
2018-07-12 13:00:30 +0530

    Create vowel_or_Consonant.py

---
+++ vowel_or_Consonant.py
@@ -0,0 +1,8 @@
+a=input()
+if( (ord(a) >=97 and ord(a) <=122) or (ord(a) >= 65 and ors(a) <=90)):
+    if(a=='a' or a=='e'or a=='i'or a=='o'or  a=='u'or a=='A' or  a=='E'or a=='I'or a=='O'or a=='U'):
+        print("Vowel")
+    else:
+        print("Consonant")
+else:
+    print("invalid")
